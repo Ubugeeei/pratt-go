110e7fa446515cd4893781676cef950982287156
Ubugeeei <ubuge1122@gamil.com>
2022-11-19 12:25:39 +0900

    feat: Node.toString

--- ast.go
+++ ast.go
@@ -7,6 +7,24 @@ type Node struct {
 	right *Node
 }
 
+func (n Node) toString() string {
+	var left string
+	if n.left == nil {
+		left = "nil"
+	} else {
+		left = n.left.toString()
+	}
+
+	var right string
+	if n.right == nil {
+		right = "nil"
+	} else {
+		right = n.right.toString()
+	}
+
+	return "{ val: " + n.val.literal + ", left: " + left + ", right: " + right + " }"
+}
+
 type NodeType int
 
 const (
