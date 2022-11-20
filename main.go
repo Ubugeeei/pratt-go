cc50ddcd001c32e55df24be2a414d9120e8e85a1
Ubugeeei <ubuge1122@gamil.com>
2022-11-20 17:26:14 +0900

    refactor

--- main.go
+++ main.go
@@ -6,58 +6,74 @@ import (
 )
 
 func main() {
-	argv := os.Args
-
-	var isInteractive bool
-	for _, arg := range argv {
-		if arg == "-i" {
-			isInteractive = true
-		}
+	if len(os.Args) == 1 {
+		fmt.Println("Usage: go run main.go [-i] [expression]")
+		os.Exit(1)
 	}
 
-	if isInteractive {
-		fmt.Println("input your expression")
-		for {
-			fmt.Print("> ")
-
-			var input string
-			fmt.Scanln(&input)
-
-			if input == "exit" {
-				fmt.Println("Bye!")
-				return
-			}
-
-			lexer := NewLexer(input)
-			parser := NewParser(lexer)
-
-			ast, e := parser.Parse()
-			if e != nil {
-				fmt.Println(e)
-			} else {
-				res, e := eval(ast)
-				if e != nil {
-					fmt.Println(e)
-				} else {
-					fmt.Println(res)
-				}
-			}
-		}
+	if isInteractive(os.Args) {
+		runInteractive()
 	} else {
-		expr := argv[1]
-		lexer := NewLexer(expr)
+		run(os.Args[1])
+	}
+}
+
+func run(expr string) {
+	lexer := NewLexer(expr)
+	parser := NewParser(lexer)
+
+	ast, e := parser.Parse()
+	if e != nil {
+		fmt.Println(e)
+		return
+	}
+
+	res, e := eval(ast)
+	if e != nil {
+		fmt.Println(e)
+		return
+	}
+
+	fmt.Println(res)
+}
+
+func runInteractive() {
+	fmt.Println("input your expression")
+	for {
+		fmt.Print("> ")
+
+		var input string
+		fmt.Scanln(&input)
+
+		if input == "exit" {
+			fmt.Println("Bye!")
+			break
+		}
+
+		lexer := NewLexer(input)
 		parser := NewParser(lexer)
 
 		ast, e := parser.Parse()
 		if e != nil {
 			fmt.Println(e)
-		} else {
-			res, e := eval(ast)
-			if e != nil {
-				fmt.Println(e)
-			} else {
-				fmt.Println(res)
-			}
+			continue
+		}
+
+		res, e := eval(ast)
+		if e != nil {
+			fmt.Println(e)
+			continue
+		}
+
+		fmt.Println(res)
+	}
+}
+
+func isInteractive(argv []string) bool {
+	for _, arg := range argv {
+		if arg == "-i" {
+			return true
 		}
 	}
+	return false
 }
